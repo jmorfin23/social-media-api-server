13477af338200f4f1bc92d55a8cc2d2ba7baae9c
Jonathan Morfin <jmorfin776@gmail.com>
2021-01-28 23:27:46 -0500

    setting up login route

--- routes.js
+++ routes.js
@@ -2,19 +2,37 @@ const express = require('express');
 const router = express.Router(); 
 const bodyParser = require('body-parser'); 
 const jsonParser = bodyParser.json(); 
+const jwt = require('jsonwebtoken'); 
 
+
+// Testing 
 const user = {
     id: 1, 
     name: 'Billy', 
     handle: 'user69', 
     url: 'https://placehold.it/250x250', 
 }
-router.get('/testroute', jsonParser, (req, res) => {
 
-    if (req.headers.user === 'user69') {
-        res.send(user)
+router.post('/login_user', jsonParser, (req, res) => {
+
+    console.log('login user route!'); 
+
+    // Verify username and password in db with sequelize ORM
+    // ...code 
+    const { username, password } = req.body.data; 
+
+    // For testing TODO: set up db w/ sequelize 
+    if (username === 'billy69' && password === 'password') {
+
+         // Create token 
+        const token = jwt.sign({ username: user.handle }, process.env.SECRET_KEY); 
+    
+        console.log('sending cooking to client'); 
+
+        // send a signed cookie 
+        res.cookie('auth', token, { maxAge: 604800000 , httpOnly: true, signed: true } ).send(); 
     } else {
-        res.send({error: 'user not found'})
+        console.log('cookie not sent'); 
     }
 }); 
 
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express'); 
 const app = express(); 
 const cors = require('cors'); 
+const cookieParser = require('cookie-parser'); 
 const PORT = process.env.PORT || 5001; 
-
+require('dotenv').config()
 
 app.use(cors()); 
+app.use(cookieParser(process.env.SECRET_KEY)); 
 
 app.get('/', (req, res) => {
-    res.json("this is the api server, received request!");
+    console.log('express api server'); 
 }); 
+
+
+// create middleware to check if the cookie is saved in the browser
+// TODO: 
+
+// if there is no cookie ... log them out 
+
+
 
 app.use('/api', require('./routes.js')); 
 
